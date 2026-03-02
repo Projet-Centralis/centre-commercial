eae248ff8ffeb734e7105c94673273645b00f191
Andry-Rakoto <andrikelinay@gmail.com>
2026-03-02 22:18:18 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://centralis-backend.onrender.com/api' // Port de votre backend
-// };
-
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api' // Port de votre backend
+  production: true,
+  apiUrl: 'https://centralis-backend.onrender.com/api' // Port de votre backend
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000/api' // Port de votre backend
+// };
