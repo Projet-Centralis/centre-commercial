6b802aae00db709b9fd090656834710e57c01a9d
Andry-Rakoto <andrikelinay@gmail.com>
2026-03-02 23:01:42 +0300

    Update production API URL to include '/api'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://centralis-backend.onrender.com' // URL de production
+  apiUrl: 'https://centralis-backend.onrender.com/api' // URL de production
 };
 
 // export const environment = {
